6fc1bab62e36273a3371955380fca8d1eb8a195a
Geneen <geneen850@gmail.com>
2025-07-17 12:08:41 +0300

    refactor: store questions as array of objects, use DOM to access elements, and add function to display questions dynamically (Relates #7)

---
+++ main.js
@@ -0,0 +1,41 @@
+const questionsData = [
+  {
+    title: 'question 1',
+    options: ['a', 'b', 'c'],
+    correctAnswer: 'a',
+  },
+  {
+    title: 'question 2',
+    options: ['A', 'B', 'C'],
+    correctAnswer: 'B',
+  },
+  {
+    title: 'question 3',
+    options: ['1', '2', '3'],
+    correctAnswer: '2',
+  },
+  {
+    title: 'question 4',
+    options: ['a', 'b', 'c'],
+    correctAnswer: 'a',
+  },
+];
+
+const questionTitle = document.querySelector('.ques-title');
+const option1 = document.querySelector('.op-1');
+const option2 = document.querySelector('.op-2');
+const option3 = document.querySelector('.op-3');
+
+let currentQesIndex = 0;
+
+const showQuestion = (index) => {
+  const question = questionsData[index];
+  questionTitle.textContent = question.title;
+  option1.textContent = question.options[0];
+  option2.textContent = question.options[1];
+  option3.textContent = question.options[2];
+};
+
+//Show first question
+showQuestion(currentQesIndex);
+
